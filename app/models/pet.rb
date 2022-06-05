bf7a023971d088b46c152081e218b9334912081b
Anand Upadhya <anand.upadhya@yahoo.com>
2022-06-05 17:44:13 +0800

    Clean up links and styling

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -1,6 +1,8 @@
 class Pet < ApplicationRecord
   SPECIES = %w(dog cat rabbit snake hamster)
-  
+
   validates :name, presence: true
+  validates :address, presence: true
+  validates :species, presence: true
   validates :species, inclusion: { in: SPECIES, message: "That doesn't look right..." }
 end

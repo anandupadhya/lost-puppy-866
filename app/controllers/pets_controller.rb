9256294ccff447c3b7cc952377dcdd498be29726
Anand Upadhya <anand.upadhya@yahoo.com>
2022-06-05 17:17:16 +0800

    Finish basic CRUD ops for Pet

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -1,2 +1,55 @@
 class PetsController < ApplicationController
+  before_action :set_pet, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @pets = Pet.all.reverse
+  end
+
+  def show
+    # @pet = Pet.find(params[:id])
+  end
+
+  def new # display a form to post a new pet
+    @pet = Pet.new
+  end
+
+  def create
+    @pet = Pet.new(pet_params)
+    @pet.found_on = Date.today
+    if @pet.save
+      redirect_to pet_path(@pet)
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+    # @pet = Pet.find(params[:id])
+  end
+
+  def update
+    # @pet = Pet.find(params[:id])
+    if @pet.update(pet_params)
+      redirect_to pet_path(@pet)
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    # @pet = Pet.find(params[:id])
+    @pet.destroy
+    redirect_to pets_path
+  end
+
+  private
+
+  def set_pet
+    @pet = Pet.find(params[:id])
+  end
+
+  def pet_params
+    params.require(:pet).permit(:name, :address, :species)
+  end
+
 end
--- app/models/pet.rb
+++ app/models/pet.rb
@@ -1,4 +1,6 @@
 class Pet < ApplicationRecord
+  SPECIES = %w(dog cat rabbit snake hamster)
+  
   validates :name, presence: true
-  validates :species, inclusion: { in: %w(dog cat rabbit snake hamster), message: "%{value} is not a valid species" }
+  validates :species, inclusion: { in: SPECIES, message: "That doesn't look right..." }
 end

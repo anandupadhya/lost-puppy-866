fa429e1897ba2ffded57ea480a950ccab7fd8ba8
Anand Upadhya <anand.upadhya@yahoo.com>
2022-06-05 14:53:49 +0800

    Add pet model, controller and seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,5 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'date'
+
+p Pet.create(name: "Hari", address: "Jianguo Rd.", species: "dog", found_on:Date.yesterday)
+p Pet.create(name: "Chabon", address: "Changning Rd.", species: "dog", found_on:Date.today)
+p Pet.create(name: "Blink", address: "Jiashan Rd.", species: "cat", found_on:Date.tomorrow)
